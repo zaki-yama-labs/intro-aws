812505477dbcd1131944ca998ef425b7e70f086d
Shingo Yamazaki <shingoyamazaki00@gmail.com>
2020-09-19 00:55:15 +0900

    4. Hands-on #1: 初めてのEC2インスタンスを起動する

--- README.md
+++ README.md
@@ -42,3 +42,30 @@ $ aws s3 "s3://${bucketName}" --force
 #### 3.6.2 AWS CDK
 - CDK: Cloud Development Kit
 - Python などのプログラミング言語を使ってCloudFormationを自動的に生成してくれるツール
+
+
+## 4. Hands-on #1: 初めてのEC2インスタンスを起動する
+
+- VPC, Security Group(SG), EC2 インスタンスの作成例
+- 4.3.2 Security Group
+
+> Security group (SG) は，EC2インスタンスに付与することのできる仮想ファイアーウォールである．例えば，特定のIPアドレスから来た接続を許したり　(インバウンド・トラフィックの制限) ，逆に特定のIPアドレスへのアクセスを禁止したり (アウトバウンド・トラフィックの制限) することができる．
+
+### :warning: ハマったところ
+
+4.4.4. デプロイを実行
+
+```
+$ cdk deploy -c key_name="XXX"
+```
+
+`-c` は context。  
+https://docs.aws.amazon.com/cdk/latest/guide/context.html
+
+context はコード中では
+
+```ts
+const keyName = this.node.tryGetContext('key_name');
+```
+
+のような形で取り出す。
--- ch04/lib/ch04-stack.ts
+++ ch04/lib/ch04-stack.ts
@@ -1,9 +1,53 @@
 import * as cdk from '@aws-cdk/core';
+import * as ec2 from '@aws-cdk/aws-ec2';
 
 export class Ch04Stack extends cdk.Stack {
   constructor(scope: cdk.Construct, id: string, props?: cdk.StackProps) {
     super(scope, id, props);
 
     // The code that defines your stack goes here
+    const subnetConfiguration: ec2.SubnetConfiguration = {
+      name: 'public',
+      subnetType: ec2.SubnetType.PUBLIC,
+    };
+    const vpc = new ec2.Vpc(this, 'MyFirstEc2-Vpc', {
+      maxAzs: 1,
+      cidr: '10.10.0.0/23',
+      subnetConfiguration: [
+        subnetConfiguration
+      ],
+      natGateways: 0,
+    });
+
+    const sg = new ec2.SecurityGroup(this, 'MyFirstEc2Vpc-Sg', {
+      vpc,
+      allowAllOutbound: true,
+    });
+    sg.addIngressRule(ec2.Peer.anyIpv4(), ec2.Port.tcp(22),
+    );
+
+    const keyName = this.node.tryGetContext('key_name');
+    console.log('keyName:', keyName);
+
+    const host = new ec2.Instance(
+      this, 'MyFirstEc2Instance',
+      {
+        instanceType: new ec2.InstanceType('t2.micro'),
+        machineImage: ec2.MachineImage.latestAmazonLinux(),
+        vpc,
+        vpcSubnets: {
+          subnetType: ec2.SubnetType.PUBLIC,
+        },
+        securityGroup: sg,
+        keyName,
+      }
+    );
+
+    new cdk.CfnOutput(this, "InstancePublicDnsName", {
+      value: host.instancePublicDnsName
+    });
+    new cdk.CfnOutput(this, "InstancePublicIp", {
+      value:host.instancePublicIp
+    });
   }
 }
